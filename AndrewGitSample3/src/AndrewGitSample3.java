3380bd0258cd637d07a123cb474c8844f03b0efb
andre <andre@192.168.0.16>
2020-11-11 21:02:48 -0500

    changed by andrew

--- AndrewGitSample3/src/AndrewGitSample3.java
+++ AndrewGitSample3/src/AndrewGitSample3.java
@@ -1,11 +1,20 @@
-
+import java.util.Scanner;
 public class AndrewGitSample3 {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println(":)");
 		int[] ar=new int[5];
+		
+		System.out.println("input array");
+			for(int i=0;i<ar.length;i++) {
+				ar[i] = sc.nextInt();
+			}	
+		for(int i=0;i<ar.length;i++)
+			System.out.print(ar[i] + " ");
 	}
 
 }
